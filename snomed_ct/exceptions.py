364e95cd110d714a3f3105311b3a469d76575e82
Arkadiusz Szydełko <arekxd@gmail.com>
2016-04-02 15:52:17 +0200

    Add SNOMED CT models.

---
+++ snomed_ct/exceptions.py
@@ -0,0 +1,3 @@
+
+
+class SNOMEDCTModelOperationNotPermitted(Exception): pass
---
+++ snomed_ct/manager.py
@@ -0,0 +1,23 @@
+from django.db.models import Manager
+
+from .exceptions import SNOMEDCTModelOperationNotPermitted
+
+
+class SNOMEDCTModelManager(Manager):
+    def create(self, *args, **kwargs):
+        raise SNOMEDCTModelOperationNotPermitted
+
+    def bulk_create(self, *args, **kwargs):
+        raise SNOMEDCTModelOperationNotPermitted
+
+    def get_or_create(self, *args, **kwargs):
+        raise SNOMEDCTModelOperationNotPermitted
+
+    def update_or_create(self, *args, **kwargs):
+        raise SNOMEDCTModelOperationNotPermitted
+
+    def delete(self, *args, **kwargs):
+        raise SNOMEDCTModelOperationNotPermitted
+
+    def update(self, *args, **kwargs):
+        raise SNOMEDCTModelOperationNotPermitted
